b6ddc9d76e8e30deefa1e6cd6c26534d0c07fbfe
Ashutosh Verma <ashutoshvrm8@gmail.com>
2019-03-17 21:45:07 +0530

    add fish

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,6 +4,18 @@ import { getFunName } from "../helpers";
 class StorePicker extends React.Component {
   myInput = React.createRef();
 
+  /**
+   * Use a constructor and bind goToStore
+   * this.goToStore = this.goToStore.bind(this)
+   * make goToStore a function goToStore (event) {}
+   * 
+   *                                  OR
+   * 
+   * Instead of declaring a method on the component (if we make goToStore a custom function)
+   * we actually declare a property which is going to be set to an arrow function
+   * and properties are bound to an instance rather than nothing then we will be able to access
+   * "this" inside of goToStore
+   */
   goToStore = event => {
     // 1. Stop the form from submitting
     event.preventDefault();
