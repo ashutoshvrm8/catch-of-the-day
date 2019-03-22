440a9ca2bf0db139d3c14852e32c22515bda8534
Ashutosh Verma <ashutoshvrm8@gmail.com>
2019-03-22 22:49:23 +0530

    made fish component and displayed state data with JSX

--- src/components/App.js
+++ src/components/App.js
@@ -38,6 +38,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+            {/* used curly brackets above to use JS inside JSX */}
           </ul>
         </div>
         <Order />
